8d9e42509b7485f54ba28f850900dcc14aab9845
Victor Daniel Gil Becerra <victordyil97@gmail.com>
2023-08-31 17:48:24 +0200

    add configurations and clean

--- src/main/java/com/victoryil/Configuration.java
+++ src/main/java/com/victoryil/Configuration.java
@@ -3,9 +3,11 @@ package com.victoryil;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
 @Data
 @NoArgsConstructor
+@Log4j
 public class Configuration {
 //Configuration to read oracle table in Spark
     private String url; //required
@@ -15,6 +17,9 @@ public class Configuration {
     private String dbTable; //required or query
 
     public void validate() {
+        log.info("Validating configuration");
+        log.info(toString());
+        System.out.println(this);
         // Validate if all required fields are present separated
         if(url == null ) {
             throw new IllegalArgumentException("url is required");
